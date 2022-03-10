451270d75d189e0d3e87b8dc44fabbad2a37e7a5
omara256 <omaraperker56@gamil.com>
2022-03-10 07:16:17 -0800

    Grammer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0 if successful, otherwise not successful
+ *
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+	return (0);
+}
