d874e90df616cd8d373182b13404665cd4cdfed4
omara256 <omaraperker56@gamil.com>
2022-03-10 06:49:36 -0800

    mad c line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main -Print Programming is like buildng a multilingual puzzle
+ * Return: 0 if successful,otherwise not 0 unsuccessful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
