38cb1865f38a09e8c8591a6be8e17088fe5b9586
omara256 <omaraperker56@gamil.com>
2022-03-15 06:06:10 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "man.h"
+
+/**
+ * main -check the code for alx school students
+ *
+ * Program that print putchar, followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
