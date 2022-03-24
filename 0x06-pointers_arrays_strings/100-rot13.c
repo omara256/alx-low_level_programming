8a64045ee0b05edf346c003298d758a3c607ed6c
omara256 <omaraperker56@gamil.com>
2022-03-24 00:55:13 -0700

    Update on Encoding rot_13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * rot13 - encodes a string in rot13
  * @s: string to be encoded
@@ -8,8 +9,10 @@
 char *rot13(char *s)
 {
 	int i, j;
+
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
