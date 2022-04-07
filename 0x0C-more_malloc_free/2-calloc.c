18c9e96fd4b7a9a3d0147959181180cf1040aa88
omara256 <omaraperker56@gamil.com>
2022-04-07 03:41:06 -0700

    Memory of an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array given number of elements and size
- * @nmemb: number of elements
- * @size: size of each element
- * Return: Null if error, else pointer to allocated memory
+ * _calloc - allocates memory set to zero for an array using malloc
+ * @nmemb: number of elements in array to be allocated for
+ * @size: size in bytes to be allocated for each element
+ * Return: pointer to allocated memory or NULL if fails or nmemb or size is 0
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
 	unsigned int i;
+	char *array;
+	unsigned int mem;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+	mem = nmemb * size;
 
-	return (p);
+	array = malloc(mem);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < mem; i++)
+		array[i] = 0;
+
+	return (array);
 }
