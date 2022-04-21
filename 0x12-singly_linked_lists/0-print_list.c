9b81e5ef5f2d9fdd64067b3912de0db76cc98f64
omara256 <omaraperker56@gamil.com>
2022-04-21 08:12:37 -0700

    Elements of a list - task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
+
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+
+	return (n);
+}
